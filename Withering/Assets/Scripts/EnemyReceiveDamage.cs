3e5f63c8995ff0aa274d77f3d2c5f6d466c9c75e
Arkhanzu <32307403+Arkhanzu@users.noreply.github.com>
2021-04-21 21:26:37 -0700

    Movement and beginning attack

---
+++ Withering/Assets/Scripts/EnemyReceiveDamage.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyReceiveDamage : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+    
+    void Start() {
+        health = maxHealth;
+    }
+
+    public void DealDamage(float damage) {
+        health -= damage;
+        CheckDeath();
+    }
+
+    private void CheckOverheal() {
+        if(health > maxHealth) {
+            health = maxHealth;
+        }
+    }
+
+    private void CheckDeath() {
+        if(health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
--- Withering/Assets/Scripts/MainMenu.cs
+++ Withering/Assets/Scripts/MainMenu.cs
@@ -5,10 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public GameObject settingsPanel;
+
     // Start is called before the first frame update
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void OpenSettings() 
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
     }
 
     // Update is called once per frame
